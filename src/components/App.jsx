7660da5fd532352dcd3ebd45e7e070333162f441
AudebertAdrien <audebertadrien.pro@gmail.com>
2021-02-06 10:47:00 +0100

    boilerPlate front

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,9 @@
-import Products from "./Products";
 import "../styles/index.scss";
 
 function App() {
   return (
     <>
-      <h1>App.jsx</h1>
-      <section className="hero"></section>
-      <Products />
+      <h1>App.jsx BoilerPlate</h1>
     </>
   );
 }
