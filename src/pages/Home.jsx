7eddaf69f1d43aabec0eac5a5e4ad17815b54709
AudebertAdrien <audebertadrien.pro@gmail.com>
2021-05-05 22:31:30 +0200

    get data from server

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,35 @@
 import axios from "axios";
 import France from "@socialgouv/react-departements";
+import { useState, useEffect } from "react";
 
 function Home() {
-  axios
-    .get("https://data-gouv-server.herokuapp.com/" | "http://localhost:3000/")
-    .then((response) => {
-      console.log(response);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+  const [incidenceData, setIncidenceData] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/")
+      .then((res) => {
+        setIncidenceData(res.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+
+  console.log(incidenceData);
   return (
     <>
       <div>Home</div>
-      <France departements={[33]} />;
+      <France departements={[33]} />
+      <div>
+        {incidenceData &&
+          incidenceData.map((item) => {
+            return <p key={item._id}>{item._id}</p>;
+          })}
+      </div>
     </>
   );
 }
 
 export default Home;
+//https://data-gouv-server.herokuapp.com/
+//http://localhost:3000/
